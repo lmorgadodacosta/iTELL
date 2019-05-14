800077a8219e777c651f74c027b08d7083762488
Sio Ut Seong <sioutseong@Sios-MacBook-Pro.local>
2019-05-14 13:38:14 +0200

    added more lists

--- game_data.py
+++ game_data.py
@@ -3,3 +3,34 @@ proverb_frames = ["My grandmother used to say...",
                   "There is an old Persian saying that goes like this...",
                   "There is an old Chinese saying that goes like this...",
                   "A wise old elf once said..."]
+
+
+#lists cannot have space
+freq_countable_noms = ["idiot", "toaster"]
+
+
+
+
+
+freq_mass_noms =  ["therapy"]
+
+
+
+
+#no multi-word expressions
+mass_noms = ["water"]
+
+
+
+
+freq_prenom_adj = ["sadistic"]
+
+
+
+
+#pay attention to capitalization
+characters = ["Ant-Man"]
+
+
+
+occupations = ["asparagus harvester"]
